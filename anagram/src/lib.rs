e3242e91b7f34008f91040dda3a295a6be7c4986
aabuelazm <abdot525@gmail.com>
2022-04-02 15:02:06 -0700

    Help

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,18 +1,37 @@
 use std::collections::HashSet;
+use unicode_segmentation::UnicodeSegmentation;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    let mut output = HashSet::new();
-    let input: &str = word.chars().map(|c| c.to_lowercase());
+pub fn anagrams_for<'a, 'b>(word: &'a str, possible_anagrams: &[&'b str]) -> HashSet<&'b str> {
+    let mut output: HashSet<&'b str> = HashSet::new();
+    let input: String = word.to_lowercase();
 
     for possible_match in possible_anagrams {
-         if is_anagram(input, possible_match.to_owned()) {
-             output.insert(*possible_match);
+        if is_anagram(&input, possible_match.to_lowercase()) {
+            output.insert(possible_match);
         }
     }
 
     output
 }
 
-fn is_anagram(input: String, possible: String) -> bool {
-    unimplemented!("Just wait")
+fn is_anagram(input: &String, possible: String) -> bool {
+    let mut copy = possible.clone();
+
+    if input.len() == copy.len() {
+        if *input != copy {
+            for c in input.graphemes(true) {
+                match copy.find(c) {
+                    Some(position) => {
+                        copy.remove(position);
+                    }
+                    None => {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+    false
 }
