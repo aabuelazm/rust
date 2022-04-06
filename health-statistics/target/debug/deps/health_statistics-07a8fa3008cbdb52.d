f7968b0e6678cd08e8d6390976d43925de41ff65
aabuelazm <62273117+aabuelazm@users.noreply.github.com>
2022-04-06 11:20:38 -0700

    Delete health-statistics/target directory

--- health-statistics/target/debug/deps/health_statistics-07a8fa3008cbdb52.d
+++
@@ -1,5 +0,0 @@
-/home/aabuelazm/exercism/rust/health-statistics/target/debug/deps/health_statistics-07a8fa3008cbdb52.rmeta: tests/health-statistics.rs
-
-/home/aabuelazm/exercism/rust/health-statistics/target/debug/deps/health_statistics-07a8fa3008cbdb52.d: tests/health-statistics.rs
-
-tests/health-statistics.rs:
