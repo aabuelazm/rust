db34992ed4c6c2d84eed679363cd1930b936b083
aabuelazm <62273117+aabuelazm@users.noreply.github.com>
2022-04-06 11:21:56 -0700

    Delete magazine-cutout/target directory

--- magazine-cutout/target/debug/deps/magazine_cutout-c2623420084a3485.d
+++
@@ -1,5 +0,0 @@
-/home/abdul/exercism/rust/magazine-cutout/target/debug/deps/magazine_cutout-c2623420084a3485.rmeta: src/lib.rs
-
-/home/abdul/exercism/rust/magazine-cutout/target/debug/deps/magazine_cutout-c2623420084a3485.d: src/lib.rs
-
-src/lib.rs:
