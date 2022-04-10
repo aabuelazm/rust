d12c66762a155c757147b8c02b4594953f9350a1
aabuelazm <62273117+aabuelazm@users.noreply.github.com>
2022-04-10 09:47:50 -0700

    Delete semi-structured-logs/target directory

--- semi-structured-logs/target/debug/deps/semi_structured_logs-55dd0bd0c34158c9.d
+++
@@ -1,5 +0,0 @@
-/home/aabuelazm/exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-55dd0bd0c34158c9: src/lib.rs
-
-/home/aabuelazm/exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-55dd0bd0c34158c9.d: src/lib.rs
-
-src/lib.rs:
--- semi-structured-logs/target/debug/deps/semi_structured_logs-e7396700b8db05e8.d
+++
@@ -1,5 +0,0 @@
-/home/aabuelazm/exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-e7396700b8db05e8.rmeta: src/lib.rs
-
-/home/aabuelazm/exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-e7396700b8db05e8.d: src/lib.rs
-
-src/lib.rs:
