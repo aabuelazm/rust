0fc04d89ee2d935e2e48a7445cd771c2449a3c79
aabuelazm <abdot525@gmail.com>
2022-04-10 10:29:28 -0700

    this excercise is bs, poorly written, and poorly implemented.

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -2,37 +2,42 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+    seconds: u64,
+}
 
 impl From<u64> for Duration {
     fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+        Duration { seconds: s }
     }
 }
 
-pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
+pub struct Planet {
+    yrs_in_earth_yrs: f64,
+}
+
+impl Planet {
+    fn years_during(&self, d: &Duration) -> f64 {
+        d.seconds as f64 / (31557600.0 * self.yrs_in_earth_yrs)
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+pub struct Earth {
+    yrs_in_earth_yrs: f64,
+}
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+// pub struct Mercury;
+// pub struct Venus;
+// pub struct Mars;
+// pub struct Jupiter;
+// pub struct Saturn;
+// pub struct Uranus;
+// pub struct Neptune;
+//
+// impl Planet for Mercury {}
+// impl Planet for Venus {}
+// impl Planet for Mars {}
+// impl Planet for Jupiter {}
+// impl Planet for Saturn {}
+// impl Planet for Uranus {}
+// impl Planet for Neptune {}
